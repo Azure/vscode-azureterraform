af4a6c52aa9230b3af64ad5cb969762789e23f87
Richard Guthrie <richardguthrie@Richards-MacBook-Pro.local>
2017-12-08 18:02:22 -0800

    added commands

--- src/extension.ts
+++ src/extension.ts
@@ -14,14 +14,26 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.plan',() => {
+            vscode.window.showInformationMessage('Hello tf.plan');
+        }),
+        vscode.commands.registerCommand('extension.init',() => {
+            vscode.window.showInformationMessage('Hello tf.init');
+        }),
+        vscode.commands.registerCommand('extension.visualize',() => {
+            vscode.window.showInformationMessage('Hello tf.visualize');
+        }),
+        vscode.commands.registerCommand('extension.execTest',() => {
+            vscode.window.showInformationMessage('Hello tf.execTest');
+        }),
+        vscode.commands.registerCommand('extension.apply',() => {
+            vscode.window.showInformationMessage('Hello tf.apply');
+        }),
+        vscode.commands.registerCommand('extension.lint',() => {
+            vscode.window.showInformationMessage('Hello tf.lint');
+        })                                
+    );
 }
 
 // this method is called when your extension is deactivated
