c7154432398abe1b164a5e59e6983b1e0b1bf2b2
Richard Guthrie <richardguthrie@Richards-MacBook-Pro.local>
2018-01-03 11:59:08 -0800

    Fixing visualize to work when in integrated or cloudshell mode.

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { extensions, commands, Disposable, window } from 'vscode';
-import { AzureAccount }from './azure-account.api';
+import { AzureAccount } from './azure-account.api';
 import { AzureServiceClient, BaseResource } from 'ms-rest-azure';
 import { CloudShell } from './cloudShell';
 import { IntegratedShell } from './integratedShell';
@@ -15,8 +15,7 @@ import { TestOption } from './utilities';
 export var CSTerminal: boolean;
 
 
-function getShell(outputChannel: vscode.OutputChannel) : BaseShell
-{
+function getShell(outputChannel: vscode.OutputChannel): BaseShell {
     var activeShell = null;
     if (CSTerminal) {
         activeShell = new CloudShell(outputChannel);
@@ -37,7 +36,7 @@ export function activate(ctx: vscode.ExtensionContext) {
     CSTerminal = (vscode.workspace.getConfiguration('tf-azure').get('terminal') == "cloudshell");
 
 
-    var outputChannel = vscode.window.createOutputChannel("VSCode extension for Azure Terraform");    
+    var outputChannel = vscode.window.createOutputChannel("VSCode extension for Azure Terraform");
     let activeShell = getShell(outputChannel);
 
     // // Capture save events and then sycn them to cloudshell if cloudshell
@@ -57,67 +56,58 @@ export function activate(ctx: vscode.ExtensionContext) {
         activeShell.runTerraformCmd("terraform init");
     }));
 
-    ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.plan', () =>{
+    ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.plan', () => {
         activeShell.runTerraformCmd("terraform plan");
     }));
 
     ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.apply', () => {
         activeShell.runTerraformCmd("terraform apply");
     }));
-    
+
     ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.destroy', () => {
         activeShell.runTerraformCmd("terraform destroy");
     }));
-    
+
     ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.refresh', () => {
         activeShell.runTerraformCmd("terraform refresh");
     }));
-    
+
     ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.validate', () => {
         activeShell.runTerraformCmd("terraform validate");
     }));
 
-    if (!CSTerminal) {
-        ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.visualize', () => {
-            var iShell : IntegratedShell;
-            iShell = <IntegratedShell> activeShell;
+    ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.visualize', () => {
+            var iShell = new IntegratedShell(outputChannel);
             iShell.visualize();
-        }));
+    }));
 
-        ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.exectest', () =>{
-            console.log('Testing current module');
+    ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.exectest', () => {
+        console.log('Testing current module');
+        // TODO - asking the type of test to run e2e or lint
+        vscode.window.showQuickPick([TestOption.lint, TestOption.e2enossh, TestOption.e2ewithssh, TestOption.custom], { placeHolder: "Select the type of test that you want to run" }).then((pick) => {
+            activeShell.runTerraformTests(pick);
+        })
+    }));
 
-
-            // TODO - asking the type of test to run e2e or lint
-            vscode.window.showQuickPick([TestOption.lint, TestOption.e2enossh, TestOption.e2ewithssh, TestOption.custom], {placeHolder: "Select the type of test that you want to run"}).then((pick) => {
-                activeShell.runTerraformTests(pick);
-            })
-
-
-
-        }));
-    }
-    else {
-        ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.push', () => {
-            // Create a function that will sync the files to Cloudshell
-            vscode.workspace.findFiles(vscode.workspace.getConfiguration('tf-azure').get('files')).then(TFfiles => {
-                activeShell.copyTerraformFiles(TFfiles);
-                });
-        }));
-    }
+    ctx.subscriptions.push(vscode.commands.registerCommand('vscode-terraform-azure.push', () => {
+        // Create a function that will sync the files to Cloudshell
+        vscode.workspace.findFiles(vscode.workspace.getConfiguration('tf-azure').get('files')).then(TFfiles => {
+            activeShell.copyTerraformFiles(TFfiles);
+        });
+    }));
 
     var dir = vscode.workspace.workspaceFolders[0].uri.fsPath;
-//     = vscode.workspace.onDidChangeTextDocument
+    //     = vscode.workspace.onDidChangeTextDocument
     // let subscriptions: Disposable[] = [];
     // vscode.window.
 
 }
 
-export async function TFLogin(api: AzureAccount){
+export async function TFLogin(api: AzureAccount) {
     console.log('entering TFLogin')
-            if (!(await api.waitForLogin())){
-                return commands.executeCommand('azure-account.askForLogin');
-            }
+    if (!(await api.waitForLogin())) {
+        return commands.executeCommand('azure-account.askForLogin');
+    }
     console.log('done TFLogin')
 }
 
