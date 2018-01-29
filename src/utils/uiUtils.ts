74c8f337646535bf58b14d7e239522a5fd4f9765
Sheng Chen <sheche@microsoft.com>
2018-01-29 10:52:43 +0800

    extract dialog buttons (#60)

--- src/utils/uiUtils.ts
+++ src/utils/uiUtils.ts
@@ -4,10 +4,14 @@ import * as opn from "opn";
 import * as vscode from "vscode";
 
 export async function openUrlHint(message: string, url: string): Promise<void> {
-    const open: vscode.MessageItem = { title: "Open" };
-    const close: vscode.MessageItem = { title: "Close", isCloseAffordance: true };
-    const response = await vscode.window.showInformationMessage(`${message} (See: ${url})`, open, close);
-    if (response === open && url) {
+    const response = await vscode.window.showInformationMessage(`${message} (See: ${url})`, DialogOption.OPEN, DialogOption.CANCEL);
+    if (response === DialogOption.OPEN && url) {
         opn(url);
     }
+}
+
+export namespace DialogOption {
+    export const OK: vscode.MessageItem = { title: "OK" };
+    export const CANCEL: vscode.MessageItem = { title: "Cancel", isCloseAffordance: true };
+    export const OPEN: vscode.MessageItem = { title: "Open" };
 }
