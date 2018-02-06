57d47d5078b3bbef902fb5b518b1b141a4548f7d
Sheng Chen <sheche@microsoft.com>
2018-02-06 11:14:17 +0800

    fix the wrong link (#99)

--- src/utils/azureUtils.ts
+++ src/utils/azureUtils.ts
@@ -7,7 +7,7 @@ export function isServicePrincipalSetInEnv(): boolean {
         process.env.ARM_TENANT_ID && process.env.ARM_TEST_LOCATION)) {
         openUrlHint(
             "Azure Service Principal is not set.",
-            "https://www.terraform.io/docs/providers/azurerm/authenticating_via_azure_cli.html",
+            "https://www.terraform.io/docs/providers/azurerm/authenticating_via_service_principal.html",
         );
         return false;
     }
