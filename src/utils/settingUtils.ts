7514e69eacee5b7515d12f6e8afceb6d70745ccd
Sheng Chen <sheche@microsoft.com>
2018-03-23 14:34:48 +0800

    remove the updateSetting() since they are not used in the current code base

--- src/utils/settingUtils.ts
+++ src/utils/settingUtils.ts
@@ -15,16 +15,6 @@ export function getSyncFileBlobPattern(): vscode.GlobPattern {
     return getTerraformExtensionSetting<vscode.GlobPattern>("files");
 }
 
-export async function updateGlobalSetting<T = string>(section: string, value: T): Promise<void> {
-    const projectConfiguration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(extensionPrefix);
-    await projectConfiguration.update(section, value, vscode.ConfigurationTarget.Global);
-}
-
-export async function updateWorkspaceSetting<T = string>(section: string, value: T, fsPath: string): Promise<void> {
-    const projectConfiguration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(extensionPrefix, vscode.Uri.file(fsPath));
-    await projectConfiguration.update(section, value);
-}
-
 function getTerraformExtensionSetting<T>(key: string, fsPath?: string): T | undefined {
     const projectConfiguration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(extensionPrefix, fsPath ? vscode.Uri.file(fsPath) : undefined);
     return projectConfiguration.get<T>(key);
