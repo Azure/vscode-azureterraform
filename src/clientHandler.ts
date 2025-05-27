a502477a5c31dd73ab7df9975a3158386f2f2dfe
Heng Lu <henglu@microsoft.com>
2025-05-27 15:22:08 +0800

    add commands to enable/disable lsp

---
+++ src/clientHandler.ts
@@ -0,0 +1,111 @@
+import * as vscode from "vscode";
+import { TelemetryReporter } from "@vscode/extension-telemetry";
+import {
+  DocumentSelector,
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  RevealOutputChannelOn,
+  ServerOptions,
+  State,
+} from "vscode-languageclient/node";
+import { ServerPath } from "./serverPath";
+import { config } from "./vscodeUtils";
+
+export interface TerraformLanguageClient {
+  client: LanguageClient;
+}
+
+/**
+ * ClientHandler maintains lifecycles of language clients
+ * based on the server's capabilities
+ */
+export class ClientHandler {
+  private tfClient: TerraformLanguageClient | undefined;
+
+  constructor(
+    private lsPath: ServerPath,
+    private outputChannel: vscode.OutputChannel,
+    private reporter: TelemetryReporter
+  ) {}
+
+  public async startClient(): Promise<void> {
+    console.log("Starting client");
+
+    this.tfClient = await this.createTerraformClient();
+    const disposable = this.tfClient.client.start();
+
+    this.reporter.sendRawTelemetryEvent("startClient", {
+      usePathToBinary: `${this.lsPath.hasCustomBinPath()}`,
+    });
+
+    return disposable;
+  }
+
+  private async createTerraformClient(): Promise<TerraformLanguageClient> {
+    const serverOptions = await this.getServerOptions();
+
+    const documentSelector: DocumentSelector = [
+      { scheme: "file", language: "terraform" },
+    ];
+
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: documentSelector,
+      initializationFailedHandler: (error) => {
+        this.reporter.sendTelemetryErrorEvent("initializationFailed", {
+          err: `${error}`,
+        });
+        return false;
+      },
+      outputChannel: this.outputChannel,
+      revealOutputChannelOn: RevealOutputChannelOn.Never,
+    };
+
+    const id = `terraform`;
+    const client = new LanguageClient(id, serverOptions, clientOptions);
+
+    client.onDidChangeState((event) => {
+      console.log(
+        `Client: ${State[event.oldState]} --> ${State[event.newState]}`
+      );
+      if (event.newState === State.Stopped) {
+        this.reporter.sendRawTelemetryEvent("stopClient");
+      }
+    });
+
+    return { client };
+  }
+
+  private async getServerOptions(): Promise<ServerOptions> {
+    const cmd = await this.lsPath.resolvedPathToBinary();
+    const serverArgs = config("azureTerraform").get<string[]>(
+      "languageServer.args",
+      []
+    );
+    const executable: Executable = {
+      command: cmd,
+      args: serverArgs,
+      options: {},
+    };
+    const serverOptions: ServerOptions = {
+      run: executable,
+      debug: executable,
+    };
+    this.outputChannel.appendLine(
+      `Launching language server: ${cmd} ${serverArgs.join(" ")}`
+    );
+    return serverOptions;
+  }
+
+  public async stopClient(): Promise<void> {
+    if (this.tfClient?.client === undefined) {
+      return;
+    }
+    await this.tfClient.client.stop();
+    console.log("Client stopped");
+  }
+
+  public getClient(): TerraformLanguageClient | undefined {
+    return this.tfClient;
+  }
+}
---
+++ src/serverPath.ts
@@ -0,0 +1,69 @@
+import * as path from "path";
+import * as vscode from "vscode";
+import * as which from "which";
+
+const INSTALL_FOLDER_NAME = "bin";
+export const CUSTOM_BIN_PATH_OPTION_NAME = "languageServer.pathToBinary";
+
+export class ServerPath {
+  private customBinPath: string | undefined;
+
+  constructor(private context: vscode.ExtensionContext) {
+    this.customBinPath = vscode.workspace
+      .getConfiguration("azureTerraform")
+      .get(CUSTOM_BIN_PATH_OPTION_NAME);
+  }
+
+  public installPath(): string {
+    return path.join(this.context.extensionPath, INSTALL_FOLDER_NAME);
+  }
+
+  public hasCustomBinPath(): boolean {
+    return !!this.customBinPath;
+  }
+
+  public binPath(): string {
+    if (this.customBinPath) {
+      return this.customBinPath;
+    }
+
+    return path.resolve(this.installPath(), this.binName());
+  }
+
+  public binName(): string {
+    if (this.customBinPath) {
+      return path.basename(this.customBinPath);
+    }
+
+    if (process.platform === "win32") {
+      return "azurerm-lsp.exe";
+    }
+    return "azurerm-lsp";
+  }
+
+  public async resolvedPathToBinary(): Promise<string> {
+    const pathToBinary = this.binPath();
+    let cmd: string;
+    try {
+      if (path.isAbsolute(pathToBinary)) {
+        await vscode.workspace.fs.stat(vscode.Uri.file(pathToBinary));
+        cmd = pathToBinary;
+      } else {
+        cmd = which.sync(pathToBinary);
+      }
+      console.log(`Found server at ${cmd}`);
+    } catch (err) {
+      let extraHint = "";
+      if (this.customBinPath) {
+        extraHint = `. Check "${CUSTOM_BIN_PATH_OPTION_NAME}" in your settings.`;
+      }
+      throw new Error(
+        `Unable to launch language server: ${
+          err instanceof Error ? err.message : err
+        }${extraHint}`
+      );
+    }
+
+    return cmd;
+  }
+}
